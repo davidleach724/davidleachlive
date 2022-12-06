1826c448ca48dc66a686026328254b416f2132b6
David Leach <david.leach@credera.com>
2022-12-06 10:25:48 -0700

    Upload profile pic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import profilepic from './profilepic.JPG'
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <a href='https://davidleachmusic.bandcamp.com/'>
+        <img src={profilepic} className="profile-pic"/>
+      </a>
+    </main>
+  )
 }
 
 export default App;
